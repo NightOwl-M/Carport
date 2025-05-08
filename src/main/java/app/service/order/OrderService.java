51842ac7947e44ef2913451ec50dd02408ced8e2
William1372 <william.ludwig.oster.dano@gmail.com>
2025-05-08 14:58:20 +0200

    Oprettet getOrdersByStatusId() i service klassen (jf. 3-lags arkitektur)

--- src/main/java/app/service/order/OrderService.java
+++ src/main/java/app/service/order/OrderService.java
@@ -6,7 +6,11 @@ import app.mapper.order.OrderMapper;
 import app.persistence.ConnectionPool;
 import io.javalin.http.Context;
 
+import java.util.List;
+
 public class OrderService {
+
+    private final OrderMapper orderMapper;
 
     public static Order createOrder(int customerId, int width, int length, String roof, String userText, ConnectionPool connectionPool) throws DatabaseException {
         Order order = new Order(customerId, width, length, roof, userText);
@@ -31,5 +35,15 @@ public class OrderService {
 
         OrderMapper.saveSessionOrder(order, connectionPool);
     }
+
+        public OrderService(OrderMapper orderMapper) {
+            this.orderMapper = orderMapper;
+        }
+
+        public List<Order> getOrdersByStatusId(int statusId) throws DatabaseException {
+
+            return orderMapper.getOrdersByStatusId(statusId);
+        }
+
 
 }
