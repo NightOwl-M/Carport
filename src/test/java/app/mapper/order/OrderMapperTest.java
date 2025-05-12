1d21acea80a0d875660f12089390626424e639fb
DavidK97 <david.700@live.dk>
2025-05-12 10:24:03 +0200

    Test script virker nu

--- src/test/java/app/mapper/order/OrderMapperTest.java
+++ src/test/java/app/mapper/order/OrderMapperTest.java
@@ -28,15 +28,15 @@ class OrderMapperTest {
     static void setupClass() {
         try (Connection connection = connectionPool.getConnection()) {
             try (Statement stmt = connection.createStatement()) {
-                // The test schema is already created, so we only need to delete/create test tables
+
                 stmt.execute("DROP TABLE IF EXISTS test.customer");
                 stmt.execute("DROP TABLE IF EXISTS test.orders");
                 stmt.execute("DROP SEQUENCE IF EXISTS test.customer_customer_id_seq CASCADE;");
                 stmt.execute("DROP SEQUENCE IF EXISTS test.orders_order_id_seq CASCADE;");
-                // Create tables as copy of original public schema structure
+
                 stmt.execute("CREATE TABLE test.customer AS (SELECT * from public.customer) WITH NO DATA");
                 stmt.execute("CREATE TABLE test.orders AS (SELECT * from public.orders) WITH NO DATA");
-                // Create sequences for auto generating id's for users and orders
+
                 stmt.execute("CREATE SEQUENCE test.customer_customer_id_seq");
                 stmt.execute("ALTER TABLE test.customer ALTER COLUMN customer_id SET DEFAULT nextval('test.customer_customer_id_seq')");
                 stmt.execute("CREATE SEQUENCE test.orders_order_id_seq");
@@ -52,21 +52,21 @@ class OrderMapperTest {
     void setUp() {
         try (Connection connection = connectionPool.getConnection()) {
             try (Statement stmt = connection.createStatement()) {
-                // Remove all rows from all tables
+
                 stmt.execute("DELETE FROM test.orders");
                 stmt.execute("DELETE FROM test.customer");
 
                 stmt.execute("INSERT INTO test.customer (customer_id, customer_name, customer_email, customer_address, customer_zipcode, customer_phone) " +
                         "VALUES  (1, 'David', 'David.800@email.dk', 'Jollen 53', 3070, '12345678'), " +
                         "(2, 'Jeppe', 'jeppe@email.dk', 'Havnevej 12', 2800, '87654321')," +
-                        "(3, 'William', 'william@email.dk', 'Skovvej 5', 4000, '11223344')");
+                        "(3, 'Dennis', 'dennis@email.dk', 'Skovvej 5', 4000, '11223344')");
 
 
                 stmt.execute("INSERT INTO test.orders (order_id, customer_id, carport_width, carport_length, roof, customer_text, admin_text, status_id, sales_price, created_at) " +
                         "VALUES (1, 1, 600, 780, 'Plasttrapezplader', 'Skal være god kvalitet', 'Bemærkning fra sælger', 1, 20000, '2025-05-06 10:10:49.630419')," +
                         "(2, 2, 600, 780, 'Plasttapezplader', 'Skal passe til huset', 'Afventer bekræftelse', 2, 15000, '2025-05-07 09:00:00')," +
                         "(3, 3, 600, 780, 'Plasttrapezplader', 'Ønsker det i sort', 'Tak for snakken', 3, 25000, '2025-05-08 12:30:15')");
-                // Set sequence to continue from the largest member_id
+
                 stmt.execute("SELECT setval('test.orders_order_id_seq', COALESCE((SELECT MAX(order_id) + 1 FROM test.orders), 1), false)");
                 stmt.execute("SELECT setval('test.customer_customer_id_seq', COALESCE((SELECT MAX(customer_id) + 1 FROM test.customer), 1), false)");
             }
